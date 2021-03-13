bbf2175a36b5e57680979ca5485892c80c72bfbd
MinhQuang <minhquang10ks@gmal.com>
2021-03-13 21:37:50 +0700

    update lai db

--- src/main/java/com/example/backend_final_project/controller/Admin/AdminProductController.java
+++ src/main/java/com/example/backend_final_project/controller/Admin/AdminProductController.java
@@ -2,14 +2,11 @@ package com.example.backend_final_project.controller.Admin;
 
 import com.example.backend_final_project.model.Product;
 import com.example.backend_final_project.model.Product_Image;
-import com.example.backend_final_project.model.Product_Property;
 import com.example.backend_final_project.model.Product_Property_Detail;
 import com.example.backend_final_project.service.Impl.ProductImageServiceImpl;
 import com.example.backend_final_project.service.Impl.ProductPropertyDetailServiceImpl;
 import com.example.backend_final_project.service.Impl.ProductPropertyServiceImpl;
 import com.example.backend_final_project.service.Impl.ProductServiceImpl;
-import com.example.backend_final_project.service.ProductPropertyDetailService;
-import com.example.backend_final_project.service.ProductPropertyService;
 import com.example.backend_final_project.service.dto.ProductRespone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,28 +15,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Controller
 @RequestMapping("admin/product")
 public class AdminProductController {
     @Autowired
-    private ProductPropertyDetailService productPropertyService;
-//
-//    @Autowired
-//    private ProductPropertyDetailServiceImpl productPropertyDetailServiceImpl;
+    private ProductServiceImpl productServiceImpl;
 
-//    @Autowired
-//    private ProductPropertyServiceImpl productImageServiceImpl;
+    @Autowired
+    private ProductPropertyDetailServiceImpl productPropertyDetailServiceImpl;
+
+    @Autowired
+    private ProductImageServiceImpl productImageServiceImpl;
 
     @GetMapping("/list")
     @ResponseBody
-    public List<Product_Property_Detail> getProductRespone(){
+    public List<ProductRespone> getProductRespone(){
+        List<ProductRespone> productResponesList = new ArrayList<ProductRespone>();
+        List<Product> product = productServiceImpl.getProductList();
+//        System.out.println(productImageServiceImpl.getProductImageByProdId(1));
+        System.out.println(productPropertyDetailServiceImpl.getProductPropertyDetailList());
+        for(int i = 0; i < product.size();i++){
+            ProductRespone productRespones = new ProductRespone();
+            List<Product_Property_Detail> product_property_details = productPropertyDetailServiceImpl.getByProductId(product.get(i).getID());
+            List<Product_Image> product_images = productImageServiceImpl.getProductImageByProdId(product.get(i).getID());
+            productRespones.setProduct(product.get(i));
+            productRespones.setProduct_Property_Detail(product_property_details);
+            productRespones.setProduct_images(product_images);
 
-//        List<ProductRespone> productResponesList = new ArrayList<ProductRespone>();
-        List<Product_Property_Detail> list = productPropertyService.getProductPropertyDetailList();
-        return list;
+            productResponesList.add(productRespones);
+        }
+
+
+        return productResponesList;
     }
-
 }
