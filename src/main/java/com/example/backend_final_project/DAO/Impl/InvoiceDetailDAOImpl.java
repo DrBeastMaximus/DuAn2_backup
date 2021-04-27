03ed6edf02236a1a7821b512c8b53c549b2a5ead
Maximus Lord aka Pino.Z <49819137+DrBeastMaximus@users.noreply.github.com>
2021-04-27 04:53:56 -0700

    Fix 4/27/2021 06:53PM

--- src/main/java/com/example/backend_final_project/DAO/Impl/InvoiceDetailDAOImpl.java
+++ src/main/java/com/example/backend_final_project/DAO/Impl/InvoiceDetailDAOImpl.java
@@ -41,7 +41,7 @@ public class InvoiceDetailDAOImpl implements InvoiceDetailDAO {
     @Override
     public List<Invoice_Detail> getInvoiceDetailListByInvoiceId(int invoiceID) {
         Session session = this.sessionFactory.openSession();
-        String hql = "FROM Invoice_Detail where Invoice.ID = :id";
+        String hql = "FROM Invoice_Detail where Invoice.ID = :id order by Created_date desc";
         Query query = session.createQuery(hql);
         query.setParameter("id", invoiceID);
         List<Invoice_Detail> list = query.list();
@@ -51,7 +51,7 @@ public class InvoiceDetailDAOImpl implements InvoiceDetailDAO {
     @Override
     public List<Invoice_Detail> getInvoiceDetailByProductId(int prodID) {
         Session session = this.sessionFactory.openSession();
-        String hql = "FROM Invoice_Detail where Product.id = :id";
+        String hql = "FROM Invoice_Detail where Product.id = :id order by Created_date desc";
         Query query = session.createQuery(hql);
         query.setParameter("id", prodID);
         List<Invoice_Detail> list = query.list();
--- src/main/java/com/example/backend_final_project/Utils/HTMLTableBuilder.java
+++ src/main/java/com/example/backend_final_project/Utils/HTMLTableBuilder.java
@@ -7,6 +7,55 @@ public class HTMLTableBuilder {
     private final StringBuilder table = new StringBuilder();
     public static String HTML_START = "<html>";
     public static String HTML_END = "</html>";
+    public static String HEAD_START = "<head>";
+    public static String HEAD_END = "</head>";
+    public static String STYLE = "<style>@import \"compass/css3\";\n" +
+            "\n" +
+            "table {\n" +
+            "  font-family: 'Arial';\n" +
+            "  margin: 25px auto;\n" +
+            "  border-collapse: collapse;\n" +
+            "  border: 1px solid #eee;\n" +
+            "  border-bottom: 2px solid #00cccc;\n" +
+            "  box-shadow: 0px 0px 20px rgba(0,0,0,0.10),\n" +
+            "     0px 10px 20px rgba(0,0,0,0.05),\n" +
+            "     0px 20px 20px rgba(0,0,0,0.05),\n" +
+            "     0px 30px 20px rgba(0,0,0,0.05);\n" +
+            "  tr {\n" +
+            "     &:hover {\n" +
+            "      background: #f4f4f4;\n" +
+            "      \n" +
+            "      td {\n" +
+            "        color: #555;\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "  th, td {\n" +
+            "    color: #999;\n" +
+            "    border: 1px solid #eee;\n" +
+            "    padding: 12px 35px;\n" +
+            "    border-collapse: collapse;\n" +
+            "  }\n" +
+            "  th {\n" +
+            "    background: #00cccc;\n" +
+            "    color: #fff;\n" +
+            "    text-transform: uppercase;\n" +
+            "    font-size: 12px;\n" +
+            "    &.last {\n" +
+            "      border-right: none;\n" +
+            "    }\n" +
+            "  }\n" +
+            "}</style>";
+    public static String JS = "<script>$('table tr').each(function(){\n" +
+            "  $(this).find('th').first().addClass('first');\n" +
+            "  $(this).find('th').last().addClass('last');\n" +
+            "  $(this).find('td').first().addClass('first');\n" +
+            "  $(this).find('td').last().addClass('last');\n" +
+            "});\n" +
+            "\n" +
+            "$('table tr').first().addClass('row-first');\n" +
+            "$('table tr').last().addClass('row-last');</script>";
+    public static String JQUERY = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js\"></script>";
     public static String TABLE_START_BORDER = "<table border=\"1\">";
     public static String TABLE_START = "<table>";
     public static String TABLE_END = "</table>";
@@ -32,6 +81,11 @@ public class HTMLTableBuilder {
             table.append("</b>");
         }
         table.append(HTML_START);
+        table.append(HEAD_START);
+        table.append(JQUERY);
+        table.append(JS);
+        table.append(STYLE);
+        table.append(HEAD_END);
         table.append(border ? TABLE_START_BORDER : TABLE_START);
         table.append(TABLE_END);
         table.append(HTML_END);
